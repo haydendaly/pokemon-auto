0a4880597e2b6fae9756f944374dd2ef38f47ccc
haydendaly <hcd36@cornell.edu>
2023-07-13 12:49:37 -0400

    add attribution property

--- main.py
+++ main.py
@@ -8,28 +8,26 @@ readme_file = "README.md"
 
 image_dict = {}
 
-# Organize images based on the root name (excluding _suffix)
 for filename in os.listdir(directory):
-    if filename.lower().endswith((".png", ".jpg", ".jpeg", ".tiff", ".webp")):
+    if filename.lower().endswith((".png", ".jpg", ".jpeg", ".tiff", ".bmp")):
         base = re.match("(.*?)(_([0-9]+))?$", os.path.splitext(filename)[0])
         root, _, _ = base.groups()
         if root not in image_dict:
             image_dict[root] = []
         image_dict[root].append(filename)
 
-# Generate the new table rows
 new_table_rows = []
-for name in sorted(image_dict.keys()):  # sort the keys before iterating
+for name in sorted(image_dict.keys()):
     images = image_dict[name]
     name_capitalized = " ".join([word.capitalize()
                                 for word in name.split("_")])
     images_md = ''.join(
-        [f'<img src="https://cdn.jsdelivr.net/gh/haydendaly/pokemon-auto/images/{image}" width="300" alt="{name_capitalized}"><br>' for image in images])
+        [f'<img src="https://cdn.jsdelivr.net/gh/haydendaly/pokemon-auto/images/{image}" width="400" alt="{name_capitalized}"><br>' for image in images])
 
-    # Read metadata from JSON file
     metadata_file = f"{metadata_dir}/{name}.json"
     name_with_link = name_capitalized
     description = ""
+    attribution = ""
     if os.path.exists(metadata_file):
         with open(metadata_file, "r") as json_file:
             metadata = json.load(json_file)
@@ -37,25 +35,29 @@ for name in sorted(image_dict.keys()):  # sort the keys before iterating
             if url != "":
                 name_with_link = f"[{name_capitalized}]({url})"
             description = metadata.get("description", "")
+            attribution_data = metadata.get("attribution", None)
 
-    table_row = f'| {name_with_link} |<br> {images_md} | {description} |\n'
+            if attribution_data:
+                attribution_name = attribution_data.get("name", "")
+                attribution_url = attribution_data.get("url", "")
+
+                if attribution_url != "":
+                    attribution = f"[{attribution_name}]({attribution_url})"
+                else:
+                    attribution = attribution_name
+
+    table_row = f'| {name_with_link} |<br> {images_md} | {description} | {attribution} |\n'
     new_table_rows.append(table_row)
 
-# Read the existing README file
 with open(readme_file, "r") as file:
     lines = file.readlines()
 
-# Find the "Autographs" section
 autographs_index = next(i for i, line in enumerate(
     lines) if line.strip() == "## Autographs")
-
-# Find the end of the existing table
 table_end_index = next(i for i, line in enumerate(
     lines[autographs_index:]) if line.strip() == "") + autographs_index
 
-# Replace the existing table with the new table
 lines = lines[:autographs_index + 4] + new_table_rows + ["\n"]
 
-# Write the updated README back to file
 with open(readme_file, "w") as file:
     file.writelines(lines)
